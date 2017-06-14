5f861b7bb02b0add03fd313d3e6f9adcc6f83b1e
saurav12994 <saurav12994@gmail.com>
2017-06-14 13:29:16 +0530

    first commit

---
+++ MYwebservice/src/java/com/saurav/ws/TestWS.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.saurav.ws;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+/**
+ *
+ * @author Sourabh
+ */
+@WebService
+@SOAPBinding(style=SOAPBinding.Style.RPC)
+public class TestWS {
+    @WebMethod
+    public String sayHello(String name)
+    {
+    return "Hello"+name;
+    }
+}
